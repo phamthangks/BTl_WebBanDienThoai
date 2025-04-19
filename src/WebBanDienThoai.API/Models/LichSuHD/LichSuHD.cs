7abb0780002aeda5801d5bf768f0f9e87441edda
phamthangks <phamthang6636@gmail.com>
2025-04-19 23:01:42 +0700

    Fix - Upload image

---
+++ src/WebBanDienThoai.API/Models/LichSuHD/LichSuHD.cs
@@ -0,0 +1,16 @@
+﻿namespace WebBanDienThoai.API.Models.LichSuHD
+{
+    public class LichSuHD
+    {
+        public string MaHoatDong { get; set; } = null!;
+
+        public string LoaiHoatDong { get; set; }
+
+        public DateTime? ThoiGianThucHien { get; set; }
+
+        public string GhiChu { get; set; }
+
+        public string TenDangNhap { get; set; }
+
+    }
+}
